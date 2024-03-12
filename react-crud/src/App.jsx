b6a873ca9667c03ed21baca78b09ab2fe6cc8fe0
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2024-03-11 22:38:18 -0300

    pre-post

--- react-crud/src/App.jsx
+++ react-crud/src/App.jsx
@@ -10,12 +10,74 @@ function App() {
   const url = "http://localhost:3000/products"
 
   const {data} = useApi(url)
+  const [nameProduct , setNameProduct] = useState(null)
+  const [princeProduct , setPrinceProduct] = useState(null)
 
-  console.log(data)
+  const deleteProduct = (id) =>{
+    console.log(id)
+  }
+
+  const updateProduct = (id) =>{
+    console.log(id)
+  }
+
+  const postUpdateProduct = async (e) => {
+    e.preventDefault()
+
+    const params = {
+      name:nameProduct,
+      prince:princeProduct
+    }
+
+    console.log(params)
+  }
 
   return (
    <div>
-      <h1>Lista de Produtos</h1>      
+      <h1>Lista de Produtos</h1>
+
+      {data ?  (  <table className='tbl__geral'>
+        <thead className='tbl__head'>
+          <th>Nome do Produto</th>
+          <th>Preço</th>
+          <th>Ação</th>
+        </thead>
+
+        <tbody className="tbl__body">
+          {data.map((item) =>(
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>
+                <button onClick={() => updateProduct(item.id)}>Editar</button>
+                <button onClick={() => deleteProduct(item.id)}>Excluir</button>
+              </td>
+           
+            </tr>
+          ))}
+        </tbody>
+      </table>)  : (
+        <p>Loading</p>
+      ) }
+    
+      <div className='add_product'>
+        <form onSubmit={postUpdateProduct}>
+          <label>Name Product
+            <input type="text"  value={nameProduct} 
+            name='name' 
+            onChange={(e)=> setNameProduct(e.target.value)} />
+          </label>
+
+          <label>Price
+            <input type="text" value={princeProduct} 
+            name='price' 
+            onChange={(e)=> setPrinceProduct(e.target.value)} />
+          </label>
+
+          <button type="submit" onClick={postUpdateProduct}>Click here</button>
+        </form>
+      </div>
+      
    </div>
   )
 }
